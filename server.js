53e8dc3e4d184c819543260c8cbd72d7c3d69de2
Vishal <singhdhamivishal@gmail.com>
2022-11-22 20:25:46 +0530

    chhanges

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 const http = require('http');
 const path = require('path');
 const { Server } = require('socket.io');
-const ACTIONS = require('./src/Actions');
+//const ACTIONS = require('./Actions');
+const ACTIONS = {
+    JOIN: 'join',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    LEAVE: 'leave',
+};
 
 const server = http.createServer(app);
 const io = new Server(server);
